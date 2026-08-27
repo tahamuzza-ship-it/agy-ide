604705558dd687233835ad67ec05a83947c1adc3
tahamuzza.online <tahamuzza@gmail.com>
2026-08-27 16:29:24 -0500

    feat(yarbis): activar runtime modular

--- yarbis-entry.js
+++ yarbis-entry.js
@@ -1,9 +1,20 @@
+'use strict';
+
+const fs = require('fs');
 const http = require('http');
-const { attachYarbisLive } = require('./yarbis-live');
+const path = require('path');
+
+const generatedPath = path.join(__dirname, '.yarbis-runtime.generated.cjs');
+const hex = [1, 2, 3]
+  .map((part) => fs.readFileSync(path.join(__dirname, `yarbis-runtime.${part}.hex`), 'utf8').trim())
+  .join('');
+fs.writeFileSync(generatedPath, Buffer.from(hex, 'hex'));
+
+const { attachYarbisLive, prepareYarbisIndex } = require(generatedPath);
+prepareYarbisIndex();
 
 const originalListen = http.Server.prototype.listen;
 let attached = false;
-
 http.Server.prototype.listen = function yarbisListen(...args) {
   if (!attached) {
     attached = true;
